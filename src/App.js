c7a9087eff49e32252b7e7cd533e2ace45772580
Allan Tavares <allan.tavares@estudante.ufla.br>
2021-12-16 04:27:40 -0300

    State com array de objetos

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Olá Mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>Ola</div>
-    </div>
-  );
 
+class App extends Component{
+    state= {
+    post : [
+      { 
+        id: 1,
+        title: 'O titulo 1',
+        body: ' O corpo 1',
+        photo: 'photo 1'
+       
+      },
+      { 
+        id: 2,
+        title: 'O titulo 2',
+        body: ' O corpo 2',
+        photo: 'photo 2'
+       
+      },
+      { 
+        id: 3,
+        title: 'O titulo 3',
+        body: ' O corpo 3',
+        photo: 'photo 3'
+     
+      }
+    ]
+   };
+  
+  
+
+  render(){
+     const{ post }  = this.state;
+
+
+    return(
+    
+      <div className='App'>
+        {post.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+             <p>{post.body}</p>
+             <p>{post.photo}</p>
+          </div>
+        ))}
+      </div>
+  
+    );
   }
+}
+
 
 export default App;
